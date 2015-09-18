4dfcfd68a5a5d4ae1cd07b19ecfe5cfabd756193
Maggie Love <maggie.love1991@gmail.com>
2015-09-18 16:56:25 -0400

    updated getWinner, added playToFive

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -38,44 +38,123 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-	var playerMove;
+	// either have var playerMove and var computerMove and take these out
+	// of parentheses (parameters) or keep in paren and take out var.
 	while (playerMove === “rock”) {
 	if (computerMove === “rock”) {
-		winner = “tie”;
+		return “tie”;
+	// Not sure this makes sense—maybe return “tie” instead/ winner not def
 	} else if (computerMove === “paper”) {
-		winner = “computer”;
+		return “computer”;
 	} else if (computerMove === “scissors”) {
-		winner = “player”;
+		return “player”;
 		}
 	}
 	while (playerMove === “paper”) {
 	if (computerMove === “rock”) {
-		winner = “player”;
+		return “player”;
 	} else if (computerMove === “paper”) {
-		winner = “tie”;
+		return “tie”;
 	} else if (computerMove === “scissors”) {
-		winner = “computer”;
+		return “computer”;
 		}
 	}
 	while (playerMove === “scissors”) {
 	if (computerMove === “rock”) {
-		winner = “computer”;
+		return “computer”;
 	} else if (computerMove === “paper”) {
-		winner = “player”;
+		return “player”;
 	} else if (computerMove === “scissors”) {
-		winner = “tie”;
+		return “tie”;
 		}
 	}
 
 }
  
+function playToFive() {
+  console.log('Let\'s play Rock Paper Scissors');
+  var playerWins = 0;
+  var computerWins = 0;
+  while (playerWins < 5 && computerWins < 5) {
+    if (winnerIsPlayer()){
+	   playerWins += 1;
+    }
+    if (winnerIsComputer()){
+	computerWins += 1; 
+    }	
+ 	console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+   console.log('The score is currently ' + playerWins + ' to ' + computerWins);   
+  return [playerWins, computerWins];
+  }
+}
 
+function winnerIsPlayer() {
+	return playerWinsRock() || playerWinsPaper() || playerWinsScissors();
+	}
+
+function winnerIsComputer() {
+	return computerWinsRock() || computerWinsPaper()|| computerWinsScissors();
+	}
+
+function playerWinsRock() {
+	return (playerMove === ‘rock’) && (computerMove === ‘scissors’);
+	}
+
+function playerWinsPaper() {
+	return (playerMove === ‘paper’) && (computerMove === ‘rock’); 
+	}
+
+function playerWinsScissors() {
+	return (playerMove === ‘scissors’) && (computerMove === ‘paper’);
+	}
+
+function computerWinsRock() {
+	return (computerMove === ‘rock’) && (playerMove === ‘scissors’);
+	}
+
+function computerWinsPaper() {
+	return (computerMove === ‘paper’) && (playerMove === ‘rock’); 
+	}
+
+function computerWinsScissors() {
+	return (computerMove === ‘scissors’) && (playerMove === ‘paper’);
+	}
+
+/* I think this version of playToFive could operate independently of the getWinner function above, but it makes logical sense to me, whereas I don’t think my attempt at incorporating getWinner function (below) would work. */
+
+
+/*
+// here is a version of playToFive that accounts for getWinner function
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    while (playerWins < 5 && computerWins < 5) {
+    //not sure while is allowed within a function.
+    if (winnerIsPlayer()){
+	playerWins += 1;
+    }
+    if (winnerIsComputer()){
+	computerWins += 1; 
+    }	
+   console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+   console.log('The score is currently ' + playerWins + ' to ' + computerWins + ‘\n’); 
+	return [playerWins, computerWins];
+  }	  
 }
+
+function winnerIsPlayer() {
+	return ‘player’;
+	// if getWinner = “player” (not sure of syntax)
+	} 	
+
+
+function winnerIsComputer() {
+	return ‘computer’;
+	// if getWinner = “computer” (not sure of syntax)
+} 
+*/
+
+
+
 
